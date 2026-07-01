64080fd6985e938437a0dede8281de1ae9d6f2f1
Luis_Ignacio <145134759+MrLuigiG00@users.noreply.github.com>
2026-06-30 18:01:45 -0600

    Component BlockNote Editor
    Component for write and preview MD like Notion

---
+++ src/components/Editor.jsx
@@ -0,0 +1,67 @@
+import "@blocknote/core/fonts/inter.css";
+import {
+  BlockNoteView,
+  darkDefaultTheme,
+  lightDefaultTheme,
+} from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+
+const lightRedTheme = {
+  colors: {
+    editor: {
+      text: "#222222",
+      background: "#ffeeee",
+    },
+    menu: {
+      text: "#ffffff",
+      background: "#9b0000",
+    },
+    tooltip: {
+      text: "#ffffff",
+      background: "#b00000",
+    },
+    hovered: {
+      text: "#ffffff",
+      background: "#b00000",
+    },
+    selected: {
+      text: "#ffffff",
+      background: "#c50000",
+    },
+    disabled: {
+      text: "#9b0000",
+      background: "#7d0000",
+    },
+    shadow: "#640000",
+    border: "#870000",
+    sideMenu: "#bababa",
+    highlights: lightDefaultTheme.colors.highlights,
+  },
+  borderRadius: 4,
+  fontFamily: "Helvetica Neue, sans-serif",
+};
+
+const darkRedTheme = {
+  ...lightRedTheme,
+  colors: {
+    ...lightRedTheme.colors,
+    editor: {
+      text: "#ffffff",
+      background: "#9b0000",
+    },
+    sideMenu: "#ffffff",
+    highlights: darkDefaultTheme.colors.highlights,
+  },
+};
+
+const redTheme = {
+  light: lightRedTheme,
+  dark: darkRedTheme,
+};
+
+export default function Editor() {
+  const editor = useCreateBlockNote({});
+
+  return <BlockNoteView editor={editor} theme={redTheme} />;
+}
